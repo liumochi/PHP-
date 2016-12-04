e8f176cf37097198c8b6b8106e3de692bdd35d1d
liumochi <m18846089770@163.com>
2016-12-04 21:03:56 +0800

    计算器有BUG

--- 5.php
+++ 5.php
@@ -5,6 +5,31 @@
  * Date: 2016/12/2
  * Time: 15:47
  */
+if(isset($_POST['sub'])){
+    $_POST['num1'];
+    $_POST['num2'];
+    $_POST['ysf'];
+    $_sum=0;
+    switch ('ysf'){
+        case '+':
+            $sum=$num1+$num2;
+            break;
+        case '-':
+            $sum=$num1-$num2;
+            break;
+        case '*':
+            $sum=$num1*$num2;
+            break;
+        case '/':
+            $sum=$num1/$num2;
+            break;
+        default:
+            echo "没有这个云算符";
+            break;
+    }
+
+}
+
 ?>
 <meta charset="utf-8">
 <html>
@@ -20,20 +45,29 @@
     </style>
 </head>
 
-<body>
-<div id="div1">
-    <form action="5.php" method="">
-        <tr>
-            <td>
-           num1:<input type="text">
-           num2:<input type="text">
-            sum:<input type="text">
-            </td>
-        </tr>
+  <body>
+    <div id="div1">
+        <table width=400 aline='center' border='1'>
+           <form action="5.php" method="">
+               <tr>
+                   <td>num1:<input type="text" name="num1" value=<?php echo $num1?>></td>
+             <td>
+                 <select name="ysf">
+                     <option value="+" <?php if($ysf=='+'){echo'selected';}?>>+</option>
+                     <option value="-" <?php if($ysf=='-'){echo'selected';}?>>-</option>
+                     <option value="*" <?php if($ysf=='*'){echo'selected';}?>>*</option>
+                     <option value="/" <?php if($ysf=='/'){echo'selected';}?>>/</option>
 
+                 </select>
 
+             </td>
+                   <td>num2:<input type="text" name="num2" value=<?php echo $num2?>></td>
+                   <td>sum:<input type="text" name="sum" value=<?php echo $sum?>></td>
+                   <td><input type="submit" name="sub" value="计算"></td>
 
-    </form>
-</div>
-</body>
+               </tr>
+           </form>
+        </table>
+    </div>
+  </body>
 </html>
