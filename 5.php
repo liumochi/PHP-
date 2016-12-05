fb230891267ade692b87777de6e1247a53ec248c
liumochi <m18846089770@163.com>
2016-12-05 23:01:36 +0800

    计算器

--- 5.php
+++ 5.php
@@ -6,11 +6,11 @@
  * Time: 15:47
  */
 if(isset($_POST['sub'])){
-    $_POST['num1'];
-    $_POST['num2'];
-    $_POST['ysf'];
-    $_sum=0;
-    switch ('ysf'){
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $ysf = $_POST['ysf'];
+    $sum=0;
+    switch ($ysf){
         case '+':
             $sum=$num1+$num2;
             break;
@@ -38,7 +38,7 @@ if(isset($_POST['sub'])){
     <style>
         #div1{
             width: 700px;
-            height: 40px;
+            height: 50px;
             background: red;
             margin:100px auto;
         }
@@ -48,21 +48,27 @@ if(isset($_POST['sub'])){
   <body>
     <div id="div1">
         <table width=400 aline='center' border='1'>
-           <form action="5.php" method="">
+           <form action="5.php" method="post">
                <tr>
-                   <td>num1:<input type="text" name="num1" value=<?php echo $num1?>></td>
+                   <td>num1:<input type="text" name="num1" value=<?php if(isset($num1))echo $num1?>></td>
              <td>
                  <select name="ysf">
-                     <option value="+" <?php if($ysf=='+'){echo'selected';}?>>+</option>
-                     <option value="-" <?php if($ysf=='-'){echo'selected';}?>>-</option>
-                     <option value="*" <?php if($ysf=='*'){echo'selected';}?>>*</option>
-                     <option value="/" <?php if($ysf=='/'){echo'selected';}?>>/</option>
+                     <option value="+" <?php
+                     if(isset($ysf)){
+                         if($ysf=='+'){
+                             echo'selected';
+                         }
+                     }
+                     ?>>+</option>
+                     <option value="-" <?php  if(isset($ysf)){if($ysf=='-'){echo'selected';}}?>>-</option>
+                     <option value="*" <?php  if(isset($ysf)){if($ysf=='*'){echo'selected';}}?>>*</option>
+                     <option value="/" <?php  if(isset($ysf)){if($ysf=='/'){echo'selected';}}?>>/</option>
 
                  </select>
 
              </td>
-                   <td>num2:<input type="text" name="num2" value=<?php echo $num2?>></td>
-                   <td>sum:<input type="text" name="sum" value=<?php echo $sum?>></td>
+                   <td>num2:<input type="text" name="num2" value=<?php if(isset($num2))echo $num2?>></td>
+                   <td>sum:<input type="text" name="sum" value=<?php if(isset($sum))echo $sum?>></td>
                    <td><input type="submit" name="sub" value="计算"></td>
 
                </tr>
