d4216ad65a6557b14cae87a0cd7b42a333e25d14
liumochi <m18846089770@163.com>
2017-01-20 21:22:17 +0800

    1.20php博客

--- ddci/application/controllers/blog.php
+++ ddci/application/controllers/blog.php
@@ -6,31 +6,31 @@
  * Time: 15:34
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
-    class Blog extends CI_Controller{
-        public function __construct()
-        {
-            parent::__construct();
-        }
-        public function index(){
-            $this->load->view('index_logined.php');
-        }
-        public function unloginindex(){
-            $data=array(
-                array(1,'老单'),
-                array(2,'小吴'),
-                array(3,'老谢'),
-            );
-            $arr['blogs']=$data;
+class Blog extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index(){
+        $this->load->view('index_logined.php');
+    }
+    public function unloginindex(){
+        $data=array(
+            array(1,'老单'),
+            array(2,'小吴'),
+            array(3,'老谢'),
+        );
+        $arr['blogs']=$data;
 
-            //var_dump($arr);
-            //die();
+        //var_dump($arr);
+        //die();
 
-            $this->load->view('index.php',$arr);
-
-        }
-        public function center(){
-            $this->load->view('adminIndex.php');
-        }
+        $this->load->view('index.php',$arr);
 
     }
+    public function center(){
+        $this->load->view('adminIndex.php');
+    }
+
+}
 ?>
