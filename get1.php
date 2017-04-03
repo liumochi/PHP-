dedf734df1cca9156cf0322c1bdffaab2247ab36
liumochi <m18846089770@163.com>
2017-04-03 22:01:28 +0800

    4.3 AJAX

---
+++ get1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/3
+ * Time: 21:02
+ */
+$username=$_GET['username'];
+$content=$_GET['content'];
+echo $username.':<br/>'.$content;
+?>
